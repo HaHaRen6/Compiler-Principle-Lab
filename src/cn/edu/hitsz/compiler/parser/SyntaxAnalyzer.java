f717fc85d98cc29a7106667e1f9bdc82249af130
hhr <huhaoran2002@qq.com>
2023-10-31 22:51:29 +0800

    lab2 ok

--- src/cn/edu/hitsz/compiler/parser/SyntaxAnalyzer.java
+++ src/cn/edu/hitsz/compiler/parser/SyntaxAnalyzer.java
@@ -2,15 +2,15 @@ package cn.edu.hitsz.compiler.parser;
 
 import cn.edu.hitsz.compiler.NotImplementedException;
 import cn.edu.hitsz.compiler.lexer.Token;
-import cn.edu.hitsz.compiler.parser.table.LRTable;
-import cn.edu.hitsz.compiler.parser.table.Production;
-import cn.edu.hitsz.compiler.parser.table.Status;
+import cn.edu.hitsz.compiler.parser.table.*;
 import cn.edu.hitsz.compiler.symtab.SymbolTable;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
+import java.util.Stack;
 
-//TODO: 实验二: 实现 LR 语法分析驱动程序
+// 实验二: 实现 LR 语法分析驱动程序
 
 /**
  * LR 语法分析驱动程序
@@ -24,6 +24,9 @@ public class SyntaxAnalyzer {
     private final SymbolTable symbolTable;
     private final List<ActionObserver> observers = new ArrayList<>();
 
+    private LRTable lrTable;
+
+    private final List<Token> myTokens = new ArrayList<>();
 
     public SyntaxAnalyzer(SymbolTable symbolTable) {
         this.symbolTable = symbolTable;
@@ -74,26 +77,76 @@ public class SyntaxAnalyzer {
         }
     }
 
+    /**
+     * hhr
+     */
     public void loadTokens(Iterable<Token> tokens) {
-        // TODO: 加载词法单元
+        // 加载词法单元
         // 你可以自行选择要如何存储词法单元, 譬如使用迭代器, 或是栈, 或是干脆使用一个 list 全存起来
         // 需要注意的是, 在实现驱动程序的过程中, 你会需要面对只读取一个 token 而不能消耗它的情况,
         // 在自行设计的时候请加以考虑此种情况
-        throw new NotImplementedException();
+        for (Token token:tokens) {
+            myTokens.add(token);
+        }
     }
 
+    /**
+     * hhr
+     */
     public void loadLRTable(LRTable table) {
-        // TODO: 加载 LR 分析表
+        // 加载 LR 分析表
         // 你可以自行选择要如何使用该表格:
         // 是直接对 LRTable 调用 getAction/getGoto, 抑或是直接将 initStatus 存起来使用
-        throw new NotImplementedException();
+        lrTable = table;
+
     }
 
+    /**
+     * hhr
+     */
     public void run() {
-        // TODO: 实现驱动程序
+        // 实现驱动程序
         // 你需要根据上面的输入来实现 LR 语法分析的驱动程序
         // 请分别在遇到 Shift, Reduce, Accept 的时候调用上面的 callWhenInShift, callWhenInReduce, callWhenInAccept
         // 否则用于为实验二打分的产生式输出可能不会正常工作
-        throw new NotImplementedException();
+        Status currentStatus = lrTable.getInit();
+        System.out.println(currentStatus);
+
+        Action action;
+
+        Stack<Status> statusStack = new Stack<>();
+        statusStack.push(currentStatus);
+
+        for (int j = 0; j < myTokens.size(); j++) {
+            Token myToken = myTokens.get(j);
+            currentStatus = statusStack.peek();
+            action = lrTable.getAction(currentStatus, myToken);
+            switch (action.getKind()) {
+                case Shift -> {
+                    callWhenInShift(currentStatus, myToken);
+                    statusStack.push(action.getStatus());
+                }
+                case Reduce -> {
+                    j--;
+                    final var production = action.getProduction();
+                    callWhenInReduce(currentStatus, production);
+                    List<String> splitProduction2Words = Arrays.stream(production.toString().split(" ")).toList();
+                    for (int i = 0; i < splitProduction2Words.size() - 2; i++) {
+                        statusStack.pop();
+                    }
+                    statusStack.push(lrTable.getGoto(statusStack.peek(), new NonTerminal(splitProduction2Words.get(0))));
+                }
+
+                case Accept -> {
+                    callWhenInAccept(currentStatus);
+                    return;
+                }
+
+                case Error -> {
+                    throw new RuntimeException("语法分析错误");
+                }
+
+            }
+        }
     }
 }
